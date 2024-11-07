70abc6bbdfc465c610ef5ab78ebf1e5e21e8a345
PhucPoo <hoangphuc1161998@gmail.com>
2024-11-07 23:07:22 +0700

    test 4

--- lib/test/event_detail_view_test.dart
+++ lib/test/event_detail_view_test.dart
@@ -1,14 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart'
-    show AppLocalizations;
-
-import 'package:event_manager/test/event_service_test.dart';
 import 'package:event_manager/test/event_model_test.dart';
+import 'package:event_manager/test/event_service_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-// ignore: unused_import
-import 'event_service_test.dart';
-
-// Màn hình chi tiết sự kiện, cho phép thêm mới hoặc cập nhật
+//view de them moi su kien
 class EventDetailView extends StatefulWidget {
   final EventModel event;
   const EventDetailView({super.key, required this.event});
@@ -24,39 +19,35 @@ class _EventDetailViewState extends State<EventDetailView> {
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
     subjectController.text = widget.event.subject;
-    notesController.text = widget.event.notes ?? '';
+    notesController.text = widget.event.notes ?? "";
   }
 
-  Future<void> _pickDateTime({required bool isStart}) async {
-    // Hiện hộp thoại chọn ngày
+  Future<void> _pickedDateTime({required bool isStart}) async {
     final pickedDate = await showDatePicker(
-      context: context,
-      initialDate: isStart ? widget.event.startTime : widget.event.endTime,
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
-
-    if (pickedDate != null) {
-      if (!mounted) return;
-      // Hiện hộp thoại chon giờ
-      final pickedTime = await showTimePicker(
         context: context,
-        initialTime: TimeOfDay.fromDateTime(
-          isStart ? widget.event.startTime : widget.event.endTime,
-        ),
-      );
-
+        initialDate: isStart ? widget.event.startTime : widget.event.endTime,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2101));
+    if (pickedDate != null) {
+      if (!mounted) {
+        return;
+      }
+      final pickedTime = await showTimePicker(
+          context: context,
+          initialTime: TimeOfDay.fromDateTime(
+              isStart ? widget.event.startTime : widget.event.endTime));
       if (pickedTime != null) {
         setState(() {
           final newDateTime = DateTime(pickedDate.year, pickedDate.month,
-              pickedDate.day, pickedTime.minute);
+              pickedDate.day, pickedTime.hour, pickedTime.minute);
           if (isStart) {
             widget.event.startTime = newDateTime;
             if (widget.event.startTime.isAfter(widget.event.endTime)) {
-              // Tự thiết lập endTime 1 tiếng sau startTime
-              widget.event.startTime.add(const Duration(hours: 1));
+              widget.event.endTime =
+                  widget.event.startTime.add(const Duration(hours: 1));
             }
           } else {
             widget.event.endTime = newDateTime;
@@ -71,79 +62,87 @@ class _EventDetailViewState extends State<EventDetailView> {
     widget.event.notes = notesController.text;
     await eventService.saveEvent(widget.event);
     if (!mounted) return;
-    Navigator.of(context).pop(true); //Trở về màn hình trước đó
+    Navigator.of(context).pop(true);
   }
 
   Future<void> _deleteEvent() async {
     await eventService.deleteEvent(widget.event);
     if (!mounted) return;
-    Navigator.of(context).pop(true); //Trở về màn hình trước đó
+    Navigator.of(context).pop(true);
   }
 
   @override
-  Widget build(BuildContext contexxt) {
+  Widget build(BuildContext context) {
     final al = AppLocalizations.of(context)!;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.event.id == null ? al.addEvent : al.eventDetails,
+        appBar: AppBar(
+          title: Text(widget.event.id == null ? al.addEvent : al.eventDetails),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: subjectController,
-              decoration: const InputDecoration(labelText: 'Tên sự kiện'),
-            ),
-            const SizedBox(height: 16),
-            ListTile(
-              title: const Text('Sự kiện cả ngày'),
-              trailing: Switch(
-                  value: widget.event.isAllDay,
-                  onChanged: (value) {
-                    setState(() {
-                      widget.event.isAllDay = value;
-                    });
-                  }),
-            ),
-            // Sử dụng toán tử trả rộng trong Dart...
-            if (!widget.event.isAllDay) ...[
-              const SizedBox(height: 16),
-              ListTile(
-                title: Text('Bắt đầu: ${widget.event.formatedStartTimeString}'),
-                trailing: const Icon(Icons.calendar_today_outlined),
-                onTap: () => _pickDateTime(isStart: true),
-              ),
-              const SizedBox(height: 16),
-              ListTile(
-                title: Text('Kết thúc ${widget.event.formatedEndTimeString}'),
-                trailing: const Icon(Icons.calendar_today_outlined),
-                onTap: () => _pickDateTime(isStart: false),
-              ),
-              TextField(
-                controller: notesController,
-                decoration: const InputDecoration(labelText: 'Ghi chú sự kiện'),
-                maxLines: 3,
-              ),
-              const SizedBox(height: 24),
-            ],
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-                // Chỉ hiển thị nút xóa nếu không phải sự kiện mới
-                if (widget.event.id != null)
-                  FilledButton.tonalIcon(
-                      onPressed: _deleteEvent,
-                      label: const Text('Xóa sự kiện')),
-                FilledButton.icon(
-                    onPressed: _saveEvent, label: const Text('Lưu sự kiện'))
+                TextField(
+                  controller: subjectController,
+                  decoration: const InputDecoration(labelText: "Tên sự kiện"),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                ListTile(
+                  title: const Text("Sự kiện cả ngày"),
+                  trailing: Switch(
+                      value: widget.event.isAllDay,
+                      onChanged: (value) {
+                        setState(() {
+                          widget.event.isAllDay = value;
+                        });
+                      }),
+                ),
+                if (!widget.event.isAllDay) ...[
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ListTile(
+                    title:
+                        Text('Bắt đầu ${widget.event.formatedStartTimeString}'),
+                    trailing: const Icon(Icons.calendar_today_outlined),
+                    onTap: () => _pickedDateTime(isStart: true),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ListTile(
+                    title:
+                        Text('Kết thúc  ${widget.event.formatedEndTimeString}'),
+                    trailing: const Icon(Icons.calendar_today_outlined),
+                    onTap: () => _pickedDateTime(isStart: false),
+                  ),
+                  TextField(
+                    controller: notesController,
+                    decoration:
+                        const InputDecoration(labelText: "Ghi chú sự kiện"),
+                    maxLength: 3,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    if (widget.event.id != null)
+                      FilledButton.tonalIcon(
+                          onPressed: _deleteEvent,
+                          label: const Text('Xoá sự kiện')),
+                    FilledButton.icon(
+                        onPressed: _saveEvent, label: const Text("Lưu sự kiện"))
+                  ],
+                )
               ],
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+          ),
+        ));
   }
 }
